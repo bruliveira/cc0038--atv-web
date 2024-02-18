2f974a7bbdeac3cc7ee4cc157673dab60337e361
bruliveira <brunaflavia910@gmail.com>
2024-02-18 15:40:46 -0300

    :tada: Add q2 - alert current url

---
+++ prova1-revisao/scripts/q2-url-alert.js
@@ -0,0 +1,4 @@
+// Escreva um programa em JavaScript que mostre o link da página web atual em uma janela do tipo alert.
+const url = window.location.href
+
+alert("Olá, segue o link da página atual: " +  url)
